dde46c448ad81afd0ef05385b7484d233f4b585b
Phoeliok <pj.subervie@gmail.com>
2018-01-29 23:58:14 +0100

    Design modification

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills float-right">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">GRLC Calculator</h3>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import Header from './Header';
+
+ReactDOM.render(
+    <div className="container">
+        <Header/>
+        <App />
+    </div>, document.getElementById('root'));
